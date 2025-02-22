c6cfb2528dbc9bafcf7b7c15d715aa946d9e479f
Kevin Cha <143443005+kevincha12@users.noreply.github.com>
2025-02-22 15:15:27 -0500

    Store location saving

--- location_reference_creation.py
+++ location_reference_creation.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 def load_walmart_csvs():
     kaggle.api.dataset_download_files('timmofeyy/-walmart-stores-location', path='./data', unzip=True)
-    walmart_store_location = pd.read_csv('./data/walmart_store_locaction.csv')
-    virginia_only = walmart_store_location[walmart_store_location['State'] == 'VA']
+    walmart_store_location = pd.read_csv('./data/walmart_store_locaction.csv', encoding='latin1')
+    virginia_only = walmart_store_location[walmart_store_location['state'] == 'VA']
     return virginia_only
 
 def load_target_csvs():
     kaggle.api.dataset_download_files('locationscloudsdata/target-store-location-data-usa', path='./data', unzip=True)
-    target_store_location = pd.read_csv('./data/target_store_locaction.csv')
+    target_store_location = pd.read_csv('./data/Target datasets.csv', encoding='latin1')
     virginia_only = target_store_location[target_store_location['Address.FormattedAddress'].str.contains(', VA ')]    
     return virginia_only
