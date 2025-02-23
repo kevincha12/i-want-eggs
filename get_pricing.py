77a564d05626727c443a6faf2f364247a078e882
Kevin Cha <143443005+kevincha12@users.noreply.github.com>
2025-02-23 01:38:10 -0500

    updated scraper to output a nice list for walmart only - target WIP

--- get_pricing.py
+++ get_pricing.py
@@ -1,18 +1,10 @@
 import location_reference_creation as LRC
-import store
+from store import Store
+from scraper import Scraper
 
 class PriceFilter:
     def __init__(self):
         self.stores = self.load_stores()
-        #egg_prices are stored in a dictionary; key is the store name, value is the price
-        self.egg_prices = self.parse_egg_prices()
-
-    def parse_egg_prices(self):
-        # TODO: implement, but basically should egg_prices
-        # should be a dictionary of address keys and price values in a list
-        # basically, String -> List<String>
-        
-        pass
 
     def load_stores(self):
         target_list = self.load_target()
@@ -23,29 +15,41 @@ class PriceFilter:
     def load_target(self):
         target_stores = []
         target_df = LRC.load_target_csvs()
+        #again, super unoptimal but for now this is a good query to just enforce and load.
+        target_optimized_url = 'https://www.target.com/s?searchTerm=eggs&tref=typeahead%7Cterm%7Ceggs%7C%7C%7Chistory&facetedValue=4pd1f&ignoreBrandExactness=true&moveTo=product-list-grid'
         for index, row in target_df.iterrows():
             full_address = row['Address.FormattedAddress']
             address_elements = [addr_elem.strip() for addr_elem in full_address.split(',')]
-            target_stores.append(store.Store(address=address_elements[0],
-                                             city=address_elements[1],
-                                             zipcode=address_elements[-1].split(' ')[1],
-                                             longitude=row['Address.Longitude'],
-                                             latitude=row['Address.Latitude'],
-                                             eggs=None))
+            #to explain, the city should be the second to last element and 
+            #the zipcode should be the last element after the split
+            address = address_elements[0]
+            city = address_elements[-2]
+            zipcode = address_elements[-1].split(' ')[1]
+            longitude = row['Address.Longitude']
+            latitude = row['Address.Latitude']
+            eggs = self.get_eggs(target_optimized_url, longitude, latitude)
+            target_stores.append(Store(address, city, zipcode, longitude, latitude, eggs))
         return target_stores
-        
 
     def load_walmarts(self):
         walmart_stores = []
         walmart_df = LRC.load_walmart_csvs()
+        #yeah it doesn't feel great but this is an optimized URI usage and then we just need to deal with long/lat so walmart picks up on the correct warehouse
+        walmart_optimized_url = 'https://www.walmart.com/search?q=egg&facet=facet_product_type%3AEggs%7C%7Cfood_form%3AWhole%7C%7Cnumber_of_pieces%3A12%7C%7Cnumber_of_pieces%3A18'
         for index, row in walmart_df.iterrows():
-            walmart_stores.append(store.Store(address=row['street_address'],
-                                              city=row['city'],
-                                              zipcode=row['zip_code'],
-                                              longitude=row['longitude'],
-                                              latitude=row['latitude'],
-                                              eggs=None))
+            address = row['street_address']
+            city = row['city']
+            zipcode = row['zip_code']
+            longitude = row['longitude']
+            latitude = row['latitude']
+            eggs = self.get_eggs(walmart_optimized_url, longitude, latitude)
+            walmart_stores.append(Store(address, city, zipcode, longitude, latitude, eggs))
         return walmart_stores
+    
+    def get_eggs(self, url, store_long, store_lat):
+        instanced_scraper = Scraper(url, store_lat, store_long)
+        instanced_scraper.scrape()
+        return instanced_scraper.read_walmart()
 
 if __name__ == '__main__':
     pf = PriceFilter()
--- scraper.py
+++ scraper.py
@@ -3,17 +3,18 @@ import requests
 from playwright.sync_api import sync_playwright
 import os
 from dotenv import load_dotenv
+import re
 
 load_dotenv()
 
 class Scraper:
-    def __init__(self, url, lat, long):
+    def __init__(self, url, long, lat):
         self.url = url
         self.html = None
         # idk how ethical this is but I'm mimicking a real browser :)
         self.ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-        self.lat = lat
         self.long = long
+        self.lat = lat
 
     #scraping using playwright - should work with proxies from oxylab :)
     def scrape(self):
@@ -25,13 +26,28 @@ class Scraper:
                                           'longitude': self.long}, permissions=['geolocation']) 
             page = context.new_page()
             page.goto(self.url)
-            #necessary to wait for the domcontent to load so we can try not to get this "Robot or Human?" stuff
-            page.wait_for_load_state('domcontentloaded')
+            #necessary to wait until all stuff has loaded :)
+            page.wait_for_load_state('domcontentloaded', timeout=60000)
             self.html = page.content()
 
     #read contents and actually receive the egg prices and stuff
-    def read(self):
+    def read_walmart(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+        spans = soup.find_all('span', class_='w_iUH7')
+        filter = [span.getText(strip=True) for span in spans if "egg" in span.text.lower() or "current price" in span.text.lower()]
+        eggs = {}
+        entry_ind = 0
+        #there should always be a product - price pair, so we can divide by 2
+        while entry_ind < int(len(filter)/2):
+            product_name = filter[entry_ind]
+            price = float(filter[entry_ind+1].split('current price $')[1])
+            eggs[product_name] = price
+            entry_ind+=2
+        return eggs
+    
+    def return_soup(self):
         return BeautifulSoup(self.html, 'html.parser')
+
 
 def main():
     #debugging to make sure this actually works :crying:
@@ -40,8 +56,9 @@ def main():
     long = -78.5706529
     scraper = Scraper(url, lat, long)
     scraper.scrape()
-    with open ('output.html', 'w') as f:
-        f.write(scraper.read().prettify())
+    print(scraper.read_walmart())
+    with open ('output.html', 'w', encoding='utf-8') as f:
+        f.write(scraper.return_soup().prettify())
 
 if __name__ == '__main__':
     main()
