c891773e916671a7d6727982546462c315465348
Kevin Cha <143443005+kevincha12@users.noreply.github.com>
2025-02-23 11:39:14 -0500

    specifying only walmart is gonna be used for now due to dynamic loading issues with target

--- get_pricing.py
+++ get_pricing.py
@@ -7,29 +7,32 @@ class PriceFilter:
         self.stores = self.load_stores()
 
     def load_stores(self):
-        target_list = self.load_target()
+        all_stores = []
+        # target_list = self.load_target()
         walmart_list = self.load_walmarts()
-        target_list.extend(walmart_list)
-        return target_list
+        # all_stores.extend(target_list)
+        all_stores.extend(walmart_list)
+        return all_stores
+        
 
-    def load_target(self):
-        target_stores = []
-        target_df = LRC.load_target_csvs()
-        #again, super unoptimal but for now this is a good query to just enforce and load.
-        target_optimized_url = 'https://www.target.com/s?searchTerm=eggs&tref=typeahead%7Cterm%7Ceggs%7C%7C%7Chistory&facetedValue=4pd1f&ignoreBrandExactness=true&moveTo=product-list-grid'
-        for index, row in target_df.iterrows():
-            full_address = row['Address.FormattedAddress']
-            address_elements = [addr_elem.strip() for addr_elem in full_address.split(',')]
-            #to explain, the city should be the second to last element and 
-            #the zipcode should be the last element after the split
-            address = address_elements[0]
-            city = address_elements[-2]
-            zipcode = address_elements[-1].split(' ')[1]
-            longitude = row['Address.Longitude']
-            latitude = row['Address.Latitude']
-            eggs = self.get_eggs(target_optimized_url, longitude, latitude)
-            target_stores.append(Store(address, city, zipcode, longitude, latitude, eggs))
-        return target_stores
+    # def load_target(self):
+    #     target_stores = []
+    #     target_df = LRC.load_target_csvs()
+    #     #again, super unoptimal but for now this is a good query to just enforce and load.
+    #     target_optimized_url = 'https://www.target.com/s?searchTerm=eggs&tref=typeahead%7Cterm%7Ceggs%7C%7C%7Chistory&facetedValue=4pd1f&ignoreBrandExactness=true&moveTo=product-list-grid'
+    #     for index, row in target_df.iterrows():
+    #         full_address = row['Address.FormattedAddress']
+    #         address_elements = [addr_elem.strip() for addr_elem in full_address.split(',')]
+    #         #to explain, the city should be the second to last element and 
+    #         #the zipcode should be the last element after the split
+    #         address = address_elements[0]
+    #         city = address_elements[-2]
+    #         zipcode = address_elements[-1].split(' ')[1]
+    #         longitude = row['Address.Longitude']
+    #         latitude = row['Address.Latitude']
+    #         eggs = self.get_eggs(target_optimized_url, longitude, latitude)
+    #         target_stores.append(Store(address, city, zipcode, longitude, latitude, eggs))
+    #     return target_stores
 
     def load_walmarts(self):
         walmart_stores = []
@@ -46,7 +49,7 @@ class PriceFilter:
             walmart_stores.append(Store(address, city, zipcode, longitude, latitude, eggs))
         return walmart_stores
     
-    def get_eggs(self, url, store_long, store_lat):
+    def get_eggs_walmart(self, url, store_long, store_lat):
         instanced_scraper = Scraper(url, store_lat, store_long)
         instanced_scraper.scrape()
         return instanced_scraper.read_walmart()
